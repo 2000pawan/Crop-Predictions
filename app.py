7f3457d3a39c91ee76f27a93e675cbe9e9176f5f
2000pawan <yaduvanshi2000pawan@gmail.com>
2024-03-17 15:13:02 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -73,9 +73,9 @@ def prediction(input):
     test_input1=pf.transform(test_input)
     predict=model.predict(test_input1)
     result=(int(((predict[0]/100)*2.47105) * 100) / 100)
-    return (f"The Production of Crop Yields:- {result} quintel/acers yield Production.
-            That means 1 acers of land produce {result} quintel of yield crop.
-            It's all depend on different Parameter like average rainfall,average temperature,soil and many more.")
+    return (f"The Production of Crop Yields:- {result} quintel/acers yield Production."
+            f"That means 1 acers of land produce {result} quintel of yield crop.",
+            "It's all depend on different Parameter like average rainfall,average temperature,soil and many more.")
 
 
 if __name__=='__main__':
